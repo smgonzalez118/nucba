e2691b08e06b6536b222edebe7cf43f1cae738da
smgonzalez118 <103335298+smgonzalez118@users.noreply.github.com>
2022-09-26 07:44:52 -0300

    Update controller.js

--- Node/integrador-node/backend/controllers/controller.js
+++ Node/integrador-node/backend/controllers/controller.js
@@ -3,58 +3,71 @@ import { min, max, median, medianAbsoluteDeviation } from 'simple-statistics';
 import { calcDate } from '../utils/dates.js';
 
 export const saveVote = async (req, res) => {
-	const { ticker, targetPriceST, targetPriceMT, targetPriceLT } = req.body;
-	const newVote = await Vote.create({
-		ticker,
-		targetPriceST,
-		targetPriceMT,
-		targetPriceLT,
-	});
-	return res
-		.status(200)
-		.json({ message: 'Voto creado exitosamente', data: newVote });
+	try {
+		const { ticker, targetPriceST, targetPriceMT, targetPriceLT } = req.body;
+		const newVote = await Vote.create({
+			ticker,
+			targetPriceST,
+			targetPriceMT,
+			targetPriceLT,
+		});
+		return res
+			.status(200)
+			.json({ message: 'Voto creado exitosamente', data: newVote });
+	} catch (err) {
+		return res.json({ message: 'Algo salió mal, reintente' });
+	}
 };
 
 export const getStats = async (req, res) => {
-	const stock = req.params.ticker;
-	const Votes = await Vote.find({ ticker: stock });
-	const STVotes = [];
-	const MTVotes = [];
-	const LTVotes = [];
+	try {
+		const stock = req.params.ticker;
+		const Votes = await Vote.find({ ticker: stock });
+		const STVotes = [];
+		const MTVotes = [];
+		const LTVotes = [];
 
-	Votes.map((vote) => {
-		STVotes.push(vote.targetPriceST);
-		MTVotes.push(vote.targetPriceMT);
-		LTVotes.push(vote.targetPriceLT);
-	});
-	const medianST = median(STVotes);
-	const medianMT = median(MTVotes);
-	const medianLT = median(LTVotes);
-	const response = {
-		medianST: medianST,
-		medianMT: medianMT,
-		medianLT: medianLT,
-	};
+		Votes.map((vote) => {
+			STVotes.push(vote.targetPriceST);
+			MTVotes.push(vote.targetPriceMT);
+			LTVotes.push(vote.targetPriceLT);
+		});
+		const medianST = median(STVotes);
+		const medianMT = median(MTVotes);
+		const medianLT = median(LTVotes);
+		const response = {
+			medianST: medianST,
+			medianMT: medianMT,
+			medianLT: medianLT,
+		};
 
-	return res
-		.status(200)
-		.json({ message: `Se han obtenido los stats de ${stock}`, data: response });
+		return res.status(200).json({
+			message: `Se han obtenido los stats de ${stock}`,
+			data: response,
+		});
+	} catch (err) {
+		return res.json({ message: 'Algo salió mal, reintente' });
+	}
 };
 
 export const getMarketPrice = async (req, res) => {
-	const stock = req.params.ticker;
-	const date = calcDate();
-	let endpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${process.env.API_KEY}`;
-	const nuevo = await fetch(endpoint);
-	const data = await nuevo.json();
-	const precio_actual = parseFloat(
-		data['Time Series (Daily)'][date]['4. close']
-	);
+	try {
+		const stock = req.params.ticker;
+		const date = calcDate();
+		let endpoint = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=${process.env.API_KEY}`;
+		const nuevo = await fetch(endpoint);
+		const data = await nuevo.json();
+		const precio_actual = parseFloat(
+			data['Time Series (Daily)'][date]['4. close']
+		);
 
-	return res.status(200).json({
-		message: `Se obtuvo el precio actual de ${stock}`,
-		data: precio_actual,
-	});
+		return res.status(200).json({
+			message: `Se obtuvo el precio actual de ${stock}`,
+			data: precio_actual,
+		});
+	} catch (err) {
+		return res.json({ message: 'Algo salió mal, reintente' });
+	}
 };
 
 // getData es una función de prueba para obtener el registro de todos los votos.
