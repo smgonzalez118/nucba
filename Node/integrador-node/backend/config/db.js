4787384f98ee20a4de02a4e0788d0cec7a7af46a
smgonzalez118 <103335298+smgonzalez118@users.noreply.github.com>
2024-01-26 12:06:11 -0300

    Delete Node/integrador-node directory

--- Node/integrador-node/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-	const conn = await mongoose.connect(process.env.DB_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-	console.log(`MongoDB conectada!!!!!: ${conn.connection.host}`);
-};
--- Node/integrador-node/backend/controllers/prueba.js
+++
@@ -1,8 +0,0 @@
-import { min, max, median, medianAbsoluteDeviation } from 'simple-statistics';
-
-const array = [50, 30, 50, 25];
-
-console.log(min(array));
-console.log(max(array));
-console.log(median(array));
-console.log(medianAbsoluteDeviation(array));
