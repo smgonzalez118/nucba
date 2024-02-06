bfb89460eb83715f3ec54493a2b2933166c403b5
Sebastian Gonzalez <smgonzalez118@gmail.com>
2024-02-05 23:34:48 -0300

    fix tools

--- Node/integrador-node/frontend/src/pages/tools.js
+++ Node/integrador-node/frontend/src/pages/tools.js
@@ -29,19 +29,20 @@ const Tools = () => {
 			.then((data) => data.json())
 			.then((data) => {
 				console.log(String(data));
-				let precio_actual = parseFloat(
-					data['Time Series (Daily)'][date]['4. close']
-				);
 				let suma = 0;
 				let contador = 0;
 				for (let fecha in data['Time Series (Daily)']) {
-					let precio = data['Time Series (Daily)'][date]['4. close'];
+					let precio = data['Time Series (Daily)'][fecha]['4. close'];
 					suma += parseFloat(precio);
 					contador += 1;
 					if (contador === 20) {
 						break;
 					}
 				}
+
+				let precio_actual = parseFloat(
+					data['Time Series (Daily)'][date]['4. close']
+				);
 
 				let precio_7d = 0;
 				let recomendacion = '';
